ccff817f80e3c0310a4edfc40e289185f6789afd
Avital Oliver <avital@thewe.net>
2016-07-30 05:22:07 +0000

    Train on new data pipeline

--- train/train.py
+++ train/train.py
@@ -11,30 +11,21 @@ from keras.callbacks import ProgbarLogger, ModelCheckpoint, EarlyStopping, Tenso
 import os
 
 samples = {
-  0: [num for num in os.listdir('../process-videos/data/0') if int(num) >= 1000],
-  1: [num for num in os.listdir('../process-videos/data/1') if int(num) >= 1000]
+  0: [file for file in os.listdir('../data/training/v0/hebrew') if file.endswith('.spectrogram.png')],
+  1: [file for file in os.listdir('../data/training/v0/english') if file.endswith('.spectrogram.png')],
 }
 num_samples = len(samples[0]) + len(samples[1])
+
+val_samples = {
+  0: [file for file in os.listdir('../data/validation/v0/hebrew') if file.endswith('.spectrogram.png')],
+  1: [file for file in os.listdir('../data/validation/v0/english') if file.endswith('.spectrogram.png')],
+}
+num_val_samples = len(val_samples[0]) + len(val_samples[1])
 
 def main():
     model = make_model()
     json_string = model.to_json()
     open('architecture.json', 'w').write(json_string)
-
-    (val_data, val_labels) = load_from_labelled_dirs(
-        '../process-videos/data/0',
-        '../process-videos/data/1',
-        max_num=1000,
-    )
-
-    def val_data_generator():
-        batch_size = 50
-        cursor = 0
-        while True:
-            batch_data = val_data[cursor:cursor+batch_size]
-            batch_labels = val_labels[cursor:cursor+batch_size]
-            cursor = (cursor + batch_size) % len(val_data)
-            yield (batch_data, batch_labels)
 
     def data_generator():
         batch_size = 128
@@ -43,10 +34,26 @@ def main():
             batch_labels = []
             for i in xrange(batch_size):
                 label = random.choice([0, 1])
-                samples_dir = '../process-videos/data/{0}'.format(label)
+                samples_dir = '../data/training/v0/{0}'.format('english' if label else 'hebrew')
                 sample = random.choice(samples[label])
-                sample_dir = '{0}/{1}'.format(samples_dir, sample)
-                image_matrix = ndimage.imread('{0}/spectrogram.png'.format(sample_dir), flatten=True)
+                spectrogram_file = '{0}/{1}'.format(samples_dir, sample)
+                image_matrix = ndimage.imread(spectrogram_file, flatten=True)
+                image_tensor = numpy.expand_dims(image_matrix, axis=0)
+                batch_data.append(image_tensor)
+                batch_labels.append(label)
+            yield (numpy.stack(batch_data), batch_labels)
+
+    def val_data_generator():
+        batch_size = 32
+        while True:
+            batch_data = []
+            batch_labels = []
+            for i in xrange(batch_size):
+                label = random.choice([0, 1])
+                samples_dir = '../data/validation/v0/{0}'.format('english' if label else 'hebrew')
+                sample = random.choice(val_samples[label])
+                spectrogram_file = '{0}/{1}'.format(samples_dir, sample)
+                image_matrix = ndimage.imread(spectrogram_file, flatten=True)
                 image_tensor = numpy.expand_dims(image_matrix, axis=0)
                 batch_data.append(image_tensor)
                 batch_labels.append(label)
@@ -57,7 +64,7 @@ def main():
         samples_per_epoch=2048,
         nb_epoch=100,
         validation_data=val_data_generator(),
-        nb_val_samples=len(val_data),
+        nb_val_samples=128,
         callbacks=[
             TensorBoard(log_dir='/mnt/nfs/logs-{0}'.format(num_samples),
                         histogram_freq=20,
@@ -65,32 +72,7 @@ def main():
         ]
     )
 
-    model.save_weights('weights.hdf5')
-
-def load_from_labelled_dirs(dir_0, dir_1, max_num=99999999999):
-    data0 = load_samples(dir_0, max_num)
-    labels0 = [0] * len(data0)
-
-    data1 = load_samples(dir_1, max_num)
-    labels1 = [1] * len(data1)
-
-    data = numpy.concatenate((data0, data1))
-    labels = labels0 + labels1
-
-    return (data, labels)
-
-def load_samples(samples_dir, max_num):
-    data = []
-    snippets = os.listdir(samples_dir)
-
-    for snippet_id in snippets:
-        if int(snippet_id) < max_num:
-            dir = '{0}/{1}'.format(samples_dir, snippet_id)
-            image_matrix = ndimage.imread('{0}/spectrogram.png'.format(dir), flatten=True)
-            image_tensor = numpy.expand_dims(image_matrix, axis=0)
-            data.append(image_tensor)
-
-    return numpy.stack(data)
+    model.save_weights('weights.hdf5', overwrite=True)
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
